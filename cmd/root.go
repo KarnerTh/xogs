b705a504443259dfeac1b1d3a3edfb0ff92acaa8
Thomas Karner <thomas.7.karner@gmail.com>
2024-12-04 08:33:28 +0100

    wip get stdin content

--- cmd/root.go
+++ cmd/root.go
@@ -1,23 +1,65 @@
 package cmd
 
 import (
+	"bufio"
+	"fmt"
 	"log/slog"
 	"os"
 
 	"github.com/KarnerTh/xogs/internal/view"
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
 	Use: "xogs",
 	Run: func(cmd *cobra.Command, args []string) {
-		view.ShowRoot()
+		// f, err := tea.LogToFile("debug.log", "debug")
+		// if err != nil {
+		// 	fmt.Println("fatal:", err)
+		// 	os.Exit(1)
+		// }
+		// defer f.Close()
+
+		p := view.CreateRootProgram()
+		go readLog(p)
+		if _, err := p.Run(); err != nil {
+			fmt.Println("Error running program:", err)
+			os.Exit(1)
+		}
 	},
 }
 
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		slog.Error(err.Error())
-		os.Exit(0)
+		os.Exit(1)
 	}
+}
+
+func readLog(p *tea.Program) {
+	if !hasStdinContent() {
+		return
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		line := scanner.Text()
+		p.Send(view.InputTest{Msg: line})
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println("error in scanning: ", err)
+		os.Exit(1)
+	}
+}
+
+func hasStdinContent() bool {
+	fi, err := os.Stdin.Stat()
+	if err != nil {
+		fmt.Println("error reading stdin: ", err)
+		return false
+	}
+
+	// source https://stackoverflow.com/a/26567513
+	return (fi.Mode() & os.ModeCharDevice) == 0
 }
--- internal/view/root.go
+++ internal/view/root.go
@@ -1,9 +1,6 @@
 package view
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/charmbracelet/bubbles/table"
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
@@ -15,6 +12,10 @@ var (
 	inputStyle   = lipgloss.NewStyle().Align(lipgloss.Left).Background(lipgloss.Color(40)).Border(lipgloss.RoundedBorder())
 	contentStyle = lipgloss.NewStyle().Align(lipgloss.Left, lipgloss.Top)
 )
+
+type InputTest struct {
+	Msg string
+}
 
 type model struct {
 	isQuitting    bool
@@ -35,7 +36,7 @@ func (m model) updateSizes(msg tea.WindowSizeMsg) model {
 
 	var cols = m.table.Columns()
 	for i, v := range cols {
-		v.Width = m.width / 5
+		v.Width = m.width/len(cols) - 2
 		cols[i] = v
 	}
 
@@ -43,7 +44,9 @@ func (m model) updateSizes(msg tea.WindowSizeMsg) model {
 	return m
 }
 
-func (m model) handleKeyPress(msg tea.KeyMsg, cmd tea.Cmd) (model, tea.Cmd) {
+func (m model) handleKeyPress(msg tea.KeyMsg) (model, tea.Cmd, bool) {
+	var preventPropergation bool
+
 	switch msg.String() {
 	case "esc":
 		if m.input.Focused() {
@@ -54,16 +57,17 @@ func (m model) handleKeyPress(msg tea.KeyMsg, cmd tea.Cmd) (model, tea.Cmd) {
 		if m.table.Focused() {
 			m.table.Blur()
 			m.input.Focus()
+			preventPropergation = true
+			return m, nil, preventPropergation
 		}
-		return m, cmd
 	case "q":
 		if !m.input.Focused() {
 			m.isQuitting = true
-			return m, tea.Quit
+			return m, tea.Quit, preventPropergation
 		}
 	case "ctrl+c":
 		m.isQuitting = true
-		return m, tea.Quit
+		return m, tea.Quit, preventPropergation
 	case "enter":
 		if m.input.Focused() {
 			m.table.Focus()
@@ -71,19 +75,24 @@ func (m model) handleKeyPress(msg tea.KeyMsg, cmd tea.Cmd) (model, tea.Cmd) {
 		}
 	}
 
-	return m, cmd
+	return m, nil, preventPropergation
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
 	switch msg := msg.(type) {
+	case InputTest:
+		m.table.SetRows(append(m.table.Rows(), table.Row{"tbd", msg.Msg}))
+		m.table.GotoBottom()
 	case tea.WindowSizeMsg:
 		m = m.updateSizes(msg)
 	case tea.KeyMsg:
-		m, cmd = m.handleKeyPress(msg, cmd)
-		if cmd != nil {
-			return m, cmd
+		model, cmd, preventPropergation := m.handleKeyPress(msg)
+		if cmd != nil || preventPropergation {
+			return model, cmd
 		}
+
+		m = model
 	}
 
 	m.table, cmd = m.table.Update(msg)
@@ -106,96 +115,14 @@ func (m model) View() string {
 	return lipgloss.JoinVertical(lipgloss.Top, header, content, input)
 }
 
-func ShowRoot() {
+func CreateRootProgram() *tea.Program {
 	columns := []table.Column{
-		{Title: "Rank", Width: 4},
-		{Title: "City", Width: 10},
-		{Title: "Country", Width: 10},
-		{Title: "Population", Width: 20},
-	}
-
-	rows := []table.Row{
-		{"1", "Tokyo", "Japan", "37,274,000"},
-		{"2", "Delhi", "India", "32,065,760"},
-		{"3", "Shanghai", "China", "28,516,904"},
-		{"4", "Dhaka", "Bangladesh", "22,478,116"},
-		{"5", "São Paulo", "Brazil", "22,429,800"},
-		{"6", "Mexico City", "Mexico", "22,085,140"},
-		{"7", "Cairo", "Egypt", "21,750,020"},
-		{"8", "Beijing", "China", "21,333,332"},
-		{"9", "Mumbai", "India", "20,961,472"},
-		{"10", "Osaka", "Japan", "19,059,856"},
-		{"11", "Chongqing", "China", "16,874,740"},
-		{"12", "Karachi", "Pakistan", "16,839,950"},
-		{"13", "Istanbul", "Turkey", "15,636,243"},
-		{"14", "Kinshasa", "DR Congo", "15,628,085"},
-		{"15", "Lagos", "Nigeria", "15,387,639"},
-		{"16", "Buenos Aires", "Argentina", "15,369,919"},
-		{"17", "Kolkata", "India", "15,133,888"},
-		{"18", "Manila", "Philippines", "14,406,059"},
-		{"19", "Tianjin", "China", "14,011,828"},
-		{"20", "Guangzhou", "China", "13,964,637"},
-		{"21", "Rio De Janeiro", "Brazil", "13,634,274"},
-		{"22", "Lahore", "Pakistan", "13,541,764"},
-		{"23", "Bangalore", "India", "13,193,035"},
-		{"24", "Shenzhen", "China", "12,831,330"},
-		{"25", "Moscow", "Russia", "12,640,818"},
-		{"26", "Chennai", "India", "11,503,293"},
-		{"27", "Bogota", "Colombia", "11,344,312"},
-		{"28", "Paris", "France", "11,142,303"},
-		{"29", "Jakarta", "Indonesia", "11,074,811"},
-		{"30", "Lima", "Peru", "11,044,607"},
-		{"31", "Bangkok", "Thailand", "10,899,698"},
-		{"32", "Hyderabad", "India", "10,534,418"},
-		{"33", "Seoul", "South Korea", "9,975,709"},
-		{"34", "Nagoya", "Japan", "9,571,596"},
-		{"35", "London", "United Kingdom", "9,540,576"},
-		{"36", "Chengdu", "China", "9,478,521"},
-		{"37", "Nanjing", "China", "9,429,381"},
-		{"38", "Tehran", "Iran", "9,381,546"},
-		{"39", "Ho Chi Minh City", "Vietnam", "9,077,158"},
-		{"40", "Luanda", "Angola", "8,952,496"},
-		{"41", "Wuhan", "China", "8,591,611"},
-		{"42", "Xi An Shaanxi", "China", "8,537,646"},
-		{"43", "Ahmedabad", "India", "8,450,228"},
-		{"44", "Kuala Lumpur", "Malaysia", "8,419,566"},
-		{"45", "New York City", "United States", "8,177,020"},
-		{"46", "Hangzhou", "China", "8,044,878"},
-		{"47", "Surat", "India", "7,784,276"},
-		{"48", "Suzhou", "China", "7,764,499"},
-		{"49", "Hong Kong", "Hong Kong", "7,643,256"},
-		{"50", "Riyadh", "Saudi Arabia", "7,538,200"},
-		{"50", "Riyadh", "Saudi Arabia", "7,538,200"},
-		{"50", "Riyadh", "Saudi Arabia", "7,538,200"},
-		{"50", "Riyadh", "Saudi Arabia", "7,538,200"},
-		{"50", "Riyadh", "Saudi Arabia", "7,538,200"},
-		{"50", "Riyadh", "Saudi Arabia", "7,538,200"},
-		{"50", "Riyadh", "Saudi Arabia", "7,538,200"},
-		{"50", "Riyadh", "Saudi Arabia", "7,538,200"},
-		{"50", "Riyadh", "Saudi Arabia", "7,538,200"},
-		{"50", "Riyadh", "Saudi Arabia", "7,538,200"},
-		{"50", "Riyadh", "Saudi Arabia", "7,538,200"},
-		{"50", "Riyadh", "Saudi Arabia", "7,538,200"},
-		{"50", "Riyadh", "Saudi Arabia", "7,538,200"},
-		{"50", "Riyadh", "Saudi Arabia", "7,538,200"},
-		{"50", "Riyadh", "Saudi Arabia", "7,538,200"},
-		{"50", "Riyadh", "Saudi Arabia", "7,538,200"},
-		{"50", "Riyadh", "Saudi Arabia", "7,538,200"},
-		{"50", "Riyadh", "Saudi Arabia", "7,538,200"},
-		{"50", "Riyadh", "Saudi Arabia", "7,538,200"},
-		{"50", "Riyadh", "Saudi Arabia", "7,538,200"},
-		{"50", "Riyadh", "Saudi Arabia", "7,538,200"},
-		{"50", "Riyadh", "Saudi Arabia", "7,538,200"},
-		{"50", "Riyadh", "Saudi Arabia", "7,538,200"},
-		{"50", "Riyadh", "Saudi Arabia", "7,538,200"},
-		{"50", "Riyadh", "Saudi Arabia", "7,538,200"},
-		{"50", "Riyadh", "Saudi Arabia", "7,538,200"},
-		{"50", "Riyadh", "Saudi Arabia", "7,538,200"},
+		{Title: "timestamp", Width: 4},
+		{Title: "log", Width: 10},
 	}
 
 	t := table.New(
 		table.WithColumns(columns),
-		table.WithRows(rows),
 		table.WithFocused(true),
 	)
 
@@ -214,9 +141,5 @@ func ShowRoot() {
 	input := textinput.New()
 	input.Placeholder = "search and filter"
 
-	m := model{table: t, input: input}
-	if _, err := tea.NewProgram(m).Run(); err != nil {
-		fmt.Println("Error running program:", err)
-		os.Exit(1)
-	}
+	return tea.NewProgram(model{table: t, input: input})
 }
