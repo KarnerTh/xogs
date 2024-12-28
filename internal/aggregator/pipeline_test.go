63de97d4ca38b96d9c0591f362becdad1b49a702
Thomas Karner <thomas.7.karner@gmail.com>
2024-12-28 12:46:04 +0100

    add unit test for remapper

--- internal/aggregator/pipeline_test.go
+++ internal/aggregator/pipeline_test.go
@@ -1,7 +1,6 @@
 package aggregator_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/KarnerTh/xogs/internal/aggregator"
@@ -12,40 +11,70 @@ import (
 
 func TestPipeline(t *testing.T) {
 	t.Parallel()
-	t.Run("json with prefix", func(t *testing.T) {
+	t.Run("parser", func(t *testing.T) {
 		t.Parallel()
-		parser := aggregator.NewPipeline([]config.Processor{
-			{
-				Parser: &config.Parser{
-					Regex: &config.ParserRegex{
-						Values: []config.ParserRegexValue{
-							{Key: "service", Regex: `\[(.*)\]`},
-							{Key: "log", Regex: `\[.*\]\s(.*)`},
+		t.Run("json with prefix", func(t *testing.T) {
+			t.Parallel()
+			// Arrange
+			pipeline := aggregator.NewPipeline([]config.Processor{
+				{
+					Parser: &config.Parser{
+						Regex: &config.ParserRegex{
+							Values: []config.ParserRegexValue{
+								{Key: "service", Regex: `\[(.*)\]`},
+								{Key: "log", Regex: `\[.*\]\s(.*)`},
+							},
 						},
 					},
 				},
-			},
-			{
-				InputKey: "log",
-				Parser:   &config.Parser{Json: &config.ParserJson{}},
-			},
-		}, parser.NewParserFactory())
+				{
+					InputKey: "log",
+					Parser:   &config.Parser{Json: &config.ParserJson{}},
+				},
+			}, parser.NewParserFactory())
+			line := `[test-service] {"level": "info", "msg": "works"}`
 
-		// Arrange
-		line := `[test-service] {"level":"info", "msg": "works"}`
+			// Act
+			log, err := pipeline.Parse(line)
 
-		// Act
-		log, err := parser.Parse(line)
-		t.Log(fmt.Sprintf("data: %+v", log.Data))
+			// Assert
+			assert.Nil(t, err)
+			assert.Equal(t, line, log.Raw)
+			assert.Equal(t, "test-service", log.Data["service"])
+			assert.Equal(t, "info", log.Data["level"])
+			assert.Equal(t, "works", log.Data["msg"])
 
-		// Assert
-		assert.Nil(t, err)
-		assert.Equal(t, line, log.Raw)
-		assert.Equal(t, "test-service", log.Data["service"])
-		assert.Equal(t, "info", log.Data["level"])
-		assert.Equal(t, "works", log.Data["msg"])
+			// make sure intermediate data from pipeline is not persisted
+			assert.NotContains(t, log.Data, "log")
+		})
+	})
 
-		// make sure intermediate data from pipeline is not persisted
-		assert.NotContains(t, log.Data, "log")
+	t.Run("remapper", func(t *testing.T) {
+		t.Parallel()
+		t.Run("remap data key", func(t *testing.T) {
+			t.Parallel()
+			// Arrange
+			pipeline := aggregator.NewPipeline([]config.Processor{
+				{
+					Parser: &config.Parser{
+						Json: &config.ParserJson{},
+					},
+				},
+				{
+					InputKey: "someLongKeyForMessage",
+					Remapper: &config.Remapper{TargetKey: "msg"},
+				},
+			}, parser.NewParserFactory())
+			line := `{"someLongKeyForMessage": "test log message"}`
+
+			// Act
+			log, err := pipeline.Parse(line)
+
+			// Assert
+			assert.Nil(t, err)
+			assert.Equal(t, line, log.Raw)
+			assert.Equal(t, "test log message", log.Data["msg"])
+			assert.NotContains(t, log.Data, "someLongKeyForMessage")
+		})
 	})
 }
